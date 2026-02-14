224fc352b427680c29187f1d0c5bd15c0ee5bda6
distrisantysas-alt <distrisantysas@gmail.com>
2026-02-14 08:45:01 -0500

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -4,4 +4,4 @@ if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL no está definida');
 }
 
-export const sql = neon(process.env.DATABASE_URL);
+export const sql = neon(process.env.DATABASE_URL!);
